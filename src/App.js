cabd9413eac1a1d56f51ea5114aea3e2ec1e7a7d
Pammy-tech <pamay0509@gmail.com>
2023-02-23 17:57:24 +0800

    Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Login from "./components/Login";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
 };
 
 export default App;
