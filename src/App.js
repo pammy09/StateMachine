7bc580c713823f655b93a45aae05a44943c25d53
Christian <pogicj32@yahoo.com>
2023-02-23 18:50:09 +0800

    state machine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useMachine } from "@xstate/react";
 import React from "react";
+import CounterMachine from "./machines/CounterMachine";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [state, send] = useMachine(CounterMachine);
+
+  return (
+    <div className="App">
+      <h1>{state.context.count}</h1>
+      <button onClick={() => send("INCREMENT")}>INCREMENT</button>
+      <button onClick={() => send("DECREMENT")}>INCREMENT</button>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/machines/CounterMachine.js
@@ -0,0 +1,23 @@
+import { assign, createMachine } from "xstate";
+
+const CounterMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QGMA2BLZBrAdAOwHsAXACwIDcwAnSAYgGEAZASXoGkBtABgF1FQADgVjoi6Ann4gAHogC0AZgBMOJQq5cArADYA7NqXaAnJoUBGADQgAnojNcjOLmYAsRlwA5NS3c49KlAF9AqzRMXAAjAFciIgl6DGxaAGUAVXp6AFFk5O4+JBAhETEJKVkEJVcnTTcXbQ8jd1cubStbBHsVTUb3fxddHwVdXWDQxMiYuLwE8NoACQB5ADVMgCVMgBE8qSLRcUkC8sqXatr6xpdm1ps7AxwFBTdNLn03XQUjEdGQQgg4KTC2B2wj2pUO8kquhwOg0mi8RiGmjadhOCm67gGXBccKMHhcLm+gNwhFIFGokGBxX2ZXkZmM0O0sPhiORHQcODcFyMZiGCmMug8hPGOGisXi40poIOoHKuk0OGGF1M71MmjprPsjk57m5vP5gpCICJOFgUWQyDg8AKuxK0pkiEeHhwHjxvXeWPqHg1zmdH16fg+ZncQvCODIlBoEEltppCEdztdnnddRdGruHiDJh5cuUmlMwWCQA */
+  createMachine({
+    context: {
+      count: 0,
+    },
+    on: {
+      INCREMENT: {
+        actions: assign({
+          count: (context) => context.count + 1,
+        }),
+      },
+      DECREMENT: {
+        actions: assign({
+          count: (context) => context.count - 1,
+        }),
+      },
+    },
+  });
+
+export default CounterMachine;
